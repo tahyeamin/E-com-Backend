68041643e8ea807a57367c9340c469ee5da0ced7
Tah Yeamin <83492183+tahyeamin@users.noreply.github.com>
2026-09-21 19:34:39 +0600

    Update products.module.ts

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { PrismaModule } from '../prisma/prisma.module'; // প্রিজমা অবশ্যই লাগবে ডাটাবেসের জন্য
-
+import { PrismaModule } from '../prisma/prisma.module'; 
 @Module({
-  imports: [PrismaModule], // ডাটাবেস এক্সেসের জন্য প্রিজমা ইম্পোর্ট করা হলো
-  controllers: [ProductsController], // কন্ট্রোলার রুটগুলো হ্যান্ডেল করবে
-  providers: [ProductsService], // সার্ভিসে আমাদের সব বিজনেস লজিক থাকবে
+  imports: [PrismaModule], 
+  controllers: [ProductsController], 
+  providers: [ProductsService], 
 })
 export class ProductsModule {}
